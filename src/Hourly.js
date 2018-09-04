1d798ad64d34910138cd22764f13d7e56035060c
Drake Bott <bott.da@gmail.com>
2018-09-04 14:52:48 -0600

    Hourly now gets data for all 7 hours and collects it

--- src/Hourly.js
+++ src/Hourly.js
@@ -2,9 +2,21 @@ import React, {Component} from 'react';
 
 class Hourly extends Component {
   render() {
+    let cards = this.props.data.forecast.simpleforecast.forecastday.map(day => {
+      let hour = day.date.hour;
+      let img = day.icon_url;
+      let avg = (day.high.fahrenheit + day.low.fahrenheit) / 2;
+      return (
+        <div className='card'>
+        <p>Hour: {hour}</p>
+        <img src={img}/>
+        <p>Avg: {avg}</p>
+        </div>);
+    });
+
     return (
       <div className="Hourly-cast">
-        <p>HOURLY CAST HERE</p>
+        { cards }
       </div>
     );
   }
