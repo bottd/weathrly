041ea9fbb2ae3be8efd780080fe4a82a1f383005
Drake Bott <bott.da@gmail.com>
2018-09-04 14:36:20 -0600

    Create Hourly component for the 7 hour forecast and add to App

--- src/App.js
+++ src/App.js
@@ -2,22 +2,16 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import Weather from './Weather';
+import Hourly from './Hourly';
 import data from './mock';
 import key from './key';
 
 class App extends Component {
   render() {
-    console.log(key);
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
         <Weather data={data}/>
+        <Hourly data={data}/>
       </div>
     );
   }
---
+++ src/Hourly.js
@@ -0,0 +1,13 @@
+import React, {Component} from 'react';
+
+class Hourly extends Component {
+  render() {
+    return (
+      <div className="Hourly-cast">
+        <p>HOURLY CAST HERE</p>
+      </div>
+    );
+  }
+}
+
+export default Hourly;
