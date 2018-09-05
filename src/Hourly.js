9bd8adc705207913db79f358942a6cad8dd70ca9
Drake Bott <bott.da@gmail.com>
2018-09-04 21:55:19 -0600

    Fix hourly to have hourly data, not daily

--- src/Hourly.js
+++ src/Hourly.js
@@ -4,10 +4,10 @@ import './Hourly.css';
 
 class Hourly extends Component {
   render() {
-    let cards = this.props.data.forecast.simpleforecast.forecastday.map(day => {
-      let hour = day.date.hour;
+    let cards = this.props.data.hourly_forecast.map(day => {
+      let hour = day.FCTTIME.hour;
       let img = day.icon_url;
-      let avg = (parseFloat(day.high.fahrenheit) + parseFloat(day.low.fahrenheit)) / 2;
+      let avg = day.temp.english;
       return (
         <div className='card'>
           <p>Hour: {hour}</p>
