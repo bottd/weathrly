89b35005cfe84054981c550dca2c9c43f574c191
Drake Bott <bott.da@gmail.com>
2018-09-11 11:45:37 -0600

    Create controls component and allow it to toggle which forecast is shown

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 import Search from './Search';
 import Card from './Card';
 import Weather from './Weather';
+import ForecastControl from './ForecastControl';
 import Hourly from './Hourly';
 import Daily from './Daily';
 import data from './mock';
@@ -12,10 +13,12 @@ class App extends Component {
   constructor() {
     super();
     this.state = {
+      hourly: true,
       mode: 'welcome',
       data
     }
     this.getData = this.getData.bind(this);
+    this.toggleHourly = this.toggleHourly.bind(this);
   }
 
   showWeather() {
@@ -39,27 +42,26 @@ class App extends Component {
             <Search getData={this.getData}/>
           </div>
           <Weather data={this.state.data} icons={this.props.icons}/>
-          <h2 className='seven-hour-forecast-label'>7 Hour Forecast:</h2>
-            <Hourly data={this.state.data} icons={this.props.icons}/>
-          <h2 className='ten-day-forecast-label'>10 Day Forecast:</h2>
-            <Daily data={this.state.data} icons={this.props.icons}/>
+          <h2 className='forecast-label'>Forecast</h2>
+          <ForecastControl toggleHourly={this.toggleHourly}/>
+          { this.state.hourly && <Hourly data={this.state.data} icons={this.props.icons}/> }
+          { !this.state.hourly && <Daily data={this.state.data} icons={this.props.icons}/> }
         </div>
-      );  
-    }    
+      );
+    }
   }
-  // componentDidMount() {
-  //   const request = `http://api.wunderground.com/api/${key}/conditions/hourly/forecast10day/q/autoip.json`;
-  //   const promise = fetch(request).then(data => data.json()).then(data => this.setState({data}));
-  // }
   getData(location) {
     location = location.split(',');
-    location[0] = location[0].replace(' ', '_');
-    const request = `http://api.wunderground.com/api/${key}/conditions/hourly/forecast10day/q/${location[1]}/${location[0]}.json`;
+    let  request = `http://api.wunderground.com/api/${key}/conditions/hourly/forecast10day/q/${location[1]}/${location[0]}.json`;
+    if (location.length === 1) {
+      request = `http://api.wunderground.com/api/${key}/conditions/hourly/forecast10day/q/${location}.json`;
+    }
     const promise = fetch(request).then(data => data.json()).then(result => this.setState({data: result, mode: 'weather'}));
     return promise;
   }
-
-
+  toggleHourly(){
+    this.setState({hourly: !this.state.hourly});
+  }
 }
 
 export default App;
