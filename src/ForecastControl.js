3687b1d77d1634117ba7f9e7f84264fae296a372
Joel Smith <joelwsmith123@gmail.com>
2018-09-11 16:29:05 -0600

    Fix forecast selector button font and add font weight indicator for disabled button

--- src/ForecastControl.js
+++ src/ForecastControl.js
@@ -5,8 +5,8 @@ class ForecastControl extends Component {
   render() {
     return (
       <div className='forecast-button-container'>
-        <button className='hourly-forecast-button' disabled={this.props.currentHourly} onClick={this.props.toggleHourly}>Hourly forecast</button>
-        <button className='daily-forecast-button' disabled={!this.props.currentHourly} onClick={this.props.toggleHourly}>10 Day forecast</button>
+        <button className='hourly-forecast-button' disabled={this.props.currentHourly} onClick={this.props.toggleHourly}>Hourly Forecast</button>
+        <button className='daily-forecast-button' disabled={!this.props.currentHourly} onClick={this.props.toggleHourly}>10 Day Forecast</button>
       </div>
     );
   }
