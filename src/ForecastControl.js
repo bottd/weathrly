9a892db5ee874473f38e18e015d0c78b147468e9
Joel Smith <joelwsmith123@gmail.com>
2018-09-11 14:43:41 -0600

    Add color confirmation for disabled forecast selector button

--- src/ForecastControl.js
+++ src/ForecastControl.js
@@ -4,9 +4,9 @@ import './ForecastControl.css';
 class ForecastControl extends Component {
   render() {
     return (
-      <div className='forecastcontrol-container'>
-        <button disabled={this.props.currentHourly} onClick={this.props.toggleHourly}>Hourly forecast</button>
-        <button disabled={!this.props.currentHourly} onClick={this.props.toggleHourly}>10 Day forecast</button>
+      <div className='forecast-button-container'>
+        <button className='hourly-forecast-button' disabled={this.props.currentHourly} onClick={this.props.toggleHourly}>Hourly forecast</button>
+        <button className='daily-forecast-button' disabled={!this.props.currentHourly} onClick={this.props.toggleHourly}>10 Day forecast</button>
       </div>
     );
   }
