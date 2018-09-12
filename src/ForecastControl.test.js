16df042a001871e96ce8e01c12e14e3b850d572b
Drake Bott <bott.da@gmail.com>
2018-09-12 14:51:21 -0600

    Add test to Forecast Control

--- src/ForecastControl.test.js
+++ src/ForecastControl.test.js
@@ -5,7 +5,11 @@ import ForecastControl from './ForecastControl';
 
 describe('Card', () => {
   it('should exist', () => {
-    const wrapper = mount(<ForecastControl/>)
-    expect(wrapper).toBeDefined()
+    const wrapper = shallow(<ForecastControl/>);
+    expect(wrapper).toBeDefined();
+  });
+  it('should have 2 buttons', () => {
+    const wrapper = shallow(<ForecastControl/>);
+    expect(wrapper.find('button').length).toEqual(2);
   });
 });
