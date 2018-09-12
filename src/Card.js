f54950703aa283b70d1c2b582c6c8650a8db272c
Drake Bott <bott.da@gmail.com>
2018-09-12 10:28:02 -0600

    Add color stripe to head of cards

--- src/Card.js
+++ src/Card.js
@@ -5,6 +5,7 @@ class Card extends Component {
   render() {
     return (
       <div className='card-container'>
+        <div className='color-label'></div>
         <h3 className='card-day'>{this.props.weekday}</h3>
         <h3 className='card-time'>{this.props.date}</h3>
         <img className='card-icon' src={this.props.img}/>
