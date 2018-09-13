7ce81ba9c5797fb8a0a30ec7f6294796df44e5e6
Drake Bott <bott.da@gmail.com>
2018-09-12 20:06:37 -0600

    Add keys to Daily cards

--- src/Daily.js
+++ src/Daily.js
@@ -4,14 +4,14 @@ import './Daily.css';
 
 class Daily extends Component {
   render() {
-    let forecast = this.props.data.forecast.simpleforecast.forecastday.map(day => {
+    let forecast = this.props.data.forecast.simpleforecast.forecastday.map((day, index) => {
       let weekday = `${day.date.weekday}`;
       let date = `${day.date.monthname_short} ${day.date.day}`;
       let weatherIcon = day.icon;
       let high = day.high.fahrenheit;
       let low = day.low.fahrenheit;
       return (
-        <Card weekday={weekday} date={date} img={this.props.icons[weatherIcon]} temp={`Hi: ${high} Low: ${low}`}/>
+        <Card weekday={weekday} date={date} img={this.props.icons[weatherIcon]} temp={`Hi: ${high} Low: ${low}`} key={index}/>
         );
     });
     return (
